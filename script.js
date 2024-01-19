ef5b27f01c4a428d5f956cc8d7490cac75d5dff3
shayaansufi <shayaansufi@gmail.com>
2024-01-19 15:15:42 -0500

    Add function that plays 1 round of rock, paper, scissors

--- script.js
+++ script.js
@@ -6,10 +6,29 @@ function getRandomInt(max) {
 function getComputerChoice() {
     number = getRandomInt(3);
     if (number === 0){
-        return "Rock";
+        return "ROCK";
     } else if (number === 1) {
-        return "Paper";
+        return "PAPER";
     } else {
-        return "Scissors";
+        return "SCISSORS";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerPick = playerSelection.toUpperCase();
+    if (playerPick === "ROCK" && computerSelection === "SCISSORS") {
+        return "You win! Rock beats Scissors";
+    } else if (playerPick === "ROCK" && computerSelection === "PAPER") {
+        return "You lose! Paper beats Rock";
+    } else if (playerPick === "PAPER" && computerSelection === "ROCK") {
+        return "You win! Paper beats Rock";
+    } else if (playerPick === "PAPER" && computerSelection === "SCISSORS") {
+        return "You lose! Scissors beats Paper";
+    } else if (playerPick === "SCISSORS" && computerSelection === "PAPER") {
+        return "You win! Scissors beats Paper";
+    } else if (playerPick === "SCISSORS" && computerSelection === "ROCK") {
+        return "You lose! Rock beats Scissors";
+    } else {
+        return "Tie!";
     }
 }
