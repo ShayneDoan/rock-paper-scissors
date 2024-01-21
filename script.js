94586aac5fa208d6a2ec8f4ffdbcbb9c374f43c8
shayaansufi <shayaansufi@gmail.com>
2024-01-21 13:25:27 -0500

    Fix script link in index.html and condense getComputerChoice function for clarity

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-
 function getComputerChoice() {
-    number = getRandomInt(3);
-    if (number === 0){
-        return "ROCK";
-    } else if (number === 1) {
-        return "PAPER";
-    } else {
-        return "SCISSORS";
+    function getRandomInt(max) {
+        return Math.ceil(Math.random() * max);
     }
+    let computerChoice;
+    switch (getRandomInt(3)) {
+        case 1:
+            computerChoice = "ROCK"
+            break;
+        case 2:
+            computerChoice = "PAPER";
+            break;
+        case 3:
+            computerChoice = "SCISSORS";
+            break;
+    }
+    return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -32,3 +35,29 @@ function playRound(playerSelection, computerSelection) {
         return "Tie!";
     }
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for(let i = 0; i < 5; i++) {
+        if (playerScore === 3) {
+            break;
+        }
+        if (computerScore === 3){
+            break;
+        }
+        let pick = prompt("Please enter your selection");
+        if (playRound(pick, getComputerChoice()) === 1) {
+            playerScore++;
+        } else if (playRound(pick, getComputerChoice()) === 0) {
+            computerScore++;
+        } else {
+            alert("You tied!");
+        }
+        console.log(playerScore);
+        console.log(computerScore);
+    }
+    return "Thanks For Playing!";
+}
+
+game();
